7b11046def11878f2aa9857cf37470ed80b678e0
Hector Valdes <hectorvaldes0808@gmail.com>
2022-10-09 22:51:13 -0500

    Implement localStorage to save data

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import DateForm from "./components/DateForm";
 import Header from "./components/Header";
 import DateList from "./components/DateList";
 
 const App = () => {
 
-  const [patients, setPatients] = useState([]);
+  const [patients, setPatients] = useState(JSON.parse(localStorage.getItem('patients')) ?? []);
   const [patient, setPatient] = useState({});
 
   const deletePatient = patientSelected => {
     const patientsUpdated = patients.filter(item => item.id !== patientSelected.id);
     setPatients(patientsUpdated);
   };
+
+  useEffect(() => {
+    localStorage.setItem('patients', JSON.stringify(patients));
+  }, [patients])
 
   return (
     <div className="container mx-auto mt-20">
